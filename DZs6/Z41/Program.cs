d6ba9e8d674cb17c304a872fb5c519649ae8788a
Artur <mail-trak96@inbox.ru>
2023-07-07 23:33:52 +0300

    добавил DZs6

---
+++ DZs6/Z41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int Result(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+System.Console.WriteLine(Result(array));
---
+++ DZs6/Z43/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double ReadInt(string text) //double, int[], string, void
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+} 
+
+
+double b1 = ReadInt("Введите b1: ");
+double k1 = ReadInt("Введите k1: ");
+double b2 = ReadInt("Введите b2: ");
+double k2 = ReadInt("Введите k2: ");
+
+
+double x = Math.Round((b2 - b1)/(k1 - k2),2);
+double y = Math.Round((k1 * x + b1),2);
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
