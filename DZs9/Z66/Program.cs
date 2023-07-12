6ef2f938a6dee7000c8c68f7dd9451aa0d4403e2
Artur <mail-trak96@inbox.ru>
2023-07-12 22:53:42 +0300

    добавил DZs9

---
+++ DZs9/Z66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int ReadInt(string text) //double, int[], string, void
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+} 
+
+int Summator(int M, int N)
+{
+    if (N < 0)
+    {
+        return 0;
+    }
+    
+    return N + Summator(M,N-1);
+}
+
+int M = ReadInt("Введите число М: ");
+int N = ReadInt("Введите число N: ");
+System.Console.WriteLine(Summator(M,N));
---
+++ DZs9/Z68/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29z
+
+
+int ReadInt(string text) //double, int[], string, void
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+} 
+
+
+
+int Akkerman(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (M > 0 & N ==0)
+    {
+        return Akkerman(M-1, 1);
+    }
+    return Akkerman(M-1,Akkerman(M,N-1));
+}
+
+
+int M = ReadInt("Введите число М: ");
+int N = ReadInt("Введите число N: ");
+System.Console.WriteLine(Akkerman(M,N));
